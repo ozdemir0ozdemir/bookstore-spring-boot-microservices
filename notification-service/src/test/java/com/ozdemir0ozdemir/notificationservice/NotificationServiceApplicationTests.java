ce37de30e985d66c2c1cfa37ba7bc51be1f65e63
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-10-01 17:57:21 +0300

    Notification Service Test Fixed

--- notification-service/src/test/java/com/ozdemir0ozdemir/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/ozdemir0ozdemir/notificationservice/NotificationServiceApplicationTests.java
@@ -1,11 +1,7 @@
 package com.ozdemir0ozdemir.notificationservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
 class NotificationServiceApplicationTests extends AbstractIT {
 
     @Test
