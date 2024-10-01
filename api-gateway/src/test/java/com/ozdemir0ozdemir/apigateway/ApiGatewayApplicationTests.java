7b01be2982cd1bd630b0d9579221b45edc70bb2b
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-10-01 19:42:58 +0300

    Api-Gateway Implemented

---
+++ api-gateway/src/test/java/com/ozdemir0ozdemir/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ozdemir0ozdemir.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
